6972047f47b3d256663acf451f13c34efc85de5f
Fredy <fredtucs7@gmail.com>
2022-06-24 18:12:20 -0500

    update script entrypoint

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-echo "The application will start in ${START_TIME_SLEEP}s..." && sleep ${START_TIME_SLEEP}
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.nttdata.microservices.transaction.TransactionServiceApplication"  "$@"
+echo "The application will start in ${START_TIME_SLEEP}s..." && sleep ${START_TIME_SLEEP};
+exec java $JAVA_OPTS -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp @/app/jib-classpath-file @/app/jib-main-class-file
